43709533ce5784f066d18a86f4eafa65514f0deb
yejibaek <qordpwl123@gmail.com>
2025-02-13 16:36:51 +0900

    view 생성

---
+++ Musico.swiftpm/View/MelodyStoryView/MelodyStoryView4.swift
@@ -0,0 +1,15 @@
+/Users/yeji/Desktop/SSCMusico1/Musico.swiftpm/View/MelodyStoryView/MelodyStoryView1.swift//
+//  MelodyStoryView1.swift
+//  Musico
+//
+//  Created by yeji on 2/13/25.
+//
+
+
+import SwiftUI
+
+struct MelodyStoryView1: View {
+    var body: some View {
+        Text("dd")
+    }
+}
---
+++ Musico.swiftpm/View/RhythmStoryView/RhythmStoryView2.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Musico
+//
+//  Created by yeji on 2/13/25.
+//
+
